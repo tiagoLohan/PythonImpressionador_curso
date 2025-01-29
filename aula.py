f50f63dce2c34c5363bb562ca994e90a97f0bbc8
Tiago Lohan <tiiagolohan@gmail.com>
2025-01-29 12:55:27 -0300

    inserido informações no arquivo 'aula.py' e incluido o arquivo '7 - Exercicios Listas - Parte 1.ipynb'

--- aula.py
+++ aula.py
@@ -1,8 +1,27 @@
 
-nome = "TiagoLohan@gmail.com"
+# nome = "TiagoLohan@gmail.com"
 
 
-print(nome[-1])
-print(len(nome))
-print(nome[:10])
-print(nome[11:])
+# print(nome[-1])
+# print(len(nome))
+# print(nome[:10])
+# print(nome[11:])
+
+
+###########################################################
+
+# Aula 7 de 14: Print e Join em Listas
+
+lista = ["Celular", "Caneta", "Bola", "Bicicleta", "Copo", "Tv", "Diploma"]
+
+
+print(lista)
+print()
+print("\n".join(lista))
+
+
+texto = "celular, caneta, bola, bicicleta, copo, tv, diploma"
+nova_lista = texto.split(", ")
+print()
+print(nova_lista)
+
