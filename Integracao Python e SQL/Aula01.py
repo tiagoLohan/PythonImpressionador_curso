d85c3cf96b971c10f67f2c4c7f5359b8fd242dae
Tiago Lohan <tiiagolohan@gmail.com>
2025-02-28 23:14:03 -0300

    Realizado exercícios de python com sql e pandas.

--- Integracao Python e SQL/Aula01.py
+++ Integracao Python e SQL/Aula01.py
@@ -1,5 +1,7 @@
 import pyodbc
-print(pyodbc.drivers())
+import pandas as pd
+import sqlite3
+# print(pyodbc.drivers()) 
 
 banco1 = "Integracao Python e SQL/salarios.sqlite"
 banco2 = "Integracao Python e SQL/chinook.db"
@@ -13,17 +15,79 @@ cursor = conexao.cursor()
 
 #  <------------ CREATE ----------->
 
-cursor.execute("""
-INSERT INTO albums (Title, ArtistId)
-VALUES
-('Lira Rock', 4)
-""")
+# cursor.execute("""
+# INSERT INTO albums (Title, ArtistId)
+# VALUES
+# ('Lira Rock', 4)
+# """)
+
+# cursor.commit()
+
+# cursor.close()
+# conexao.close()
+
+#  <------------ CREATE ----------->
+
+#  <------------ READ ----------->
+
+# cursor.execute("""
+# SELECT * FROM customers
+# """)
+
+# valores = cursor.fetchall()
+# descricao = cursor.description
+# colunas = [ coluna[0] for coluna in descricao]
+
+
+# tabela_clientes = pd.DataFrame.from_records(valores, columns=colunas)
+
+# print(tabela_clientes)
+
+
+# cursor.close()
+# conexao.close()
+
+
+#  <------------ READ ----------->
+
+#  <------------ READ COM PANDAS ----------->
+
+# conexao = sqlite3.connect(banco2)
+
+# tabela_cientes  = pd.read_sql("SELECT * FROM customers", conexao)
+
+# print(tabela_cientes)
+
+# conexao.close()
+
+#  <------------ READ COM PANDAS ----------->
+
+#  <------------ UPDATE ----------->
+
+# cursor.execute("""
+# UPDATE customers SET Email='lira@embraer.com.br' WHERE Email='luisg@embraer.com.br'
+# """)
+
+
+# cursor.commit()
+# cursor.close()
+# conexao.close()
+
+
+#  <------------ UPDATE ----------->
+
+#  <------------ DELETE ----------->
+
+cursor.execute('''
+DELETE FROM albums WHERE AlbumId=2
+''')
+
+
 
 cursor.commit()
-
-
-
-
-
 cursor.close()
 conexao.close()
+
+#  <------------ DELETE ----------->
+
+
